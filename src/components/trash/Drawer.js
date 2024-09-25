e996cb18ab177589f9e4d62df40475cf2411858b
PiyushLunawat <103507195+PiyushLunawat@users.noreply.github.com>
2024-09-25 16:22:29 +0530

    minor

--- src/components/trash/Drawer.js
+++ src/components/trash/Drawer.js
@@ -6,7 +6,7 @@ const Drawer = ({ isOpen, toggleDrawer, children }) => {
       {isOpen && (
         <div className="fixed inset-0 bg-black bg-opacity-50" onClick={toggleDrawer} ></div>
       )}
-      <div className={`fixed inset-x-0 bottom-0 transform transition-transform duration-300 ${
+      <div className={`fixed z-50 inset-x-0 bottom-0 transform transition-transform duration-300 ${
           isOpen ? "translate-y-0" : "translate-y-full" } bg-black shadow-lg`} style={{ minHeight: "200px" }} >
         <div className="border-t-8 border-green-500 w-[30vw] mx-auto"></div>
         <div className="mt-4 text-white">{children}</div>
